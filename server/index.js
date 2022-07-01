3a345500d6481c0c68a1c1bc4bc2cdbc7e41469f
abhik-b <abhik0301@gmail.com>
2022-07-01 09:32:15 +0530

    basic setup of server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+
+
+app.listen(port, () => {  
+  console.log(`Server is running on port ${port}`);
+});
