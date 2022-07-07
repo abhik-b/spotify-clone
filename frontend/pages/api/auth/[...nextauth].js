db592448ec3e04a83032c4a4ac624013e5c5512d
abhik-b <abhik0301@gmail.com>
2022-07-07 14:26:17 +0530

    google & fb login added

---
+++ frontend/pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.NEXT_AUTH_GOOGLE_CLIENT_ID,
+            clientSecret: process.env.NEXT_AUTH_GOOGLE_CLIENT_SECRET
+        }),
+        FacebookProvider({
+            clientId: process.env.NEXT_AUTH_FACEBOOK_ID,
+            clientSecret: process.env.NEXT_AUTH_FACEBOOK_SECRET
+        })
+    ]
+})
